ddb4673c7b81273bc00c4ca322aa46f55b68132e
Armando Estrada <libryum33@hotmail.com>
2020-09-22 19:32:43 -0700

    updated server.js file

---
+++ seed.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_trackerdb;
+
+CREATE DATABASE employee_trackerdb;
+
+USE  employee_trackerdb;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,2) NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department(id),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+id INT NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL, 
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT NULL,
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id),
+PRIMARY KEY (id)
+);
--- server.js
+++ server.js
@@ -1,70 +1,148 @@
 const mysql = require("mysql");
 const cTable = require("console.table");
-const inquirer =  require("inquirer");
+const inquirer = require("inquirer");
 
 //const Employee = require("Employee");
 
 
 const connectionConfig = {
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "root",
-  database: "employee_trackerdb"
+    // Your password
+    password: "root",
+    database: "employee_trackerdb"
 };
+
+
+// connection.connect(function(){
+// connection.query(
+//})
+//  function(error, date) {}
+// )
 
 const connection = mysql.createConnection(connectionConfig);
 
-const newEmployee = [];
 
-startToDo();
+connection.connect(function (err) {
+    if (err) throw err
+    startToDo();
+});
+
+
 
 function startToDo() {
-        inquirer.prompt([
-            {
-                type: "list"
-            },
-            {
-                name: "todo"
-            },
-            {
-                message: "What would you like to do?"
-            },
-            {
-                choices: [
-                   "View all employees",
-                   "View all employees by department",
-                   "View all employees by role",
-                   "Add employee",
-                   "Finished",
-                ]
-            }
-        
-        ]).then(function(response){
-            if (response.startToDo === "View all employees") {
+    inquirer.prompt([
+        {
+            type: "list"
+        },
+        {
+            name: "todo"
+        },
+        {
+            message: "What would you like to do?"
+        },
+        {
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by role",
+                "Add employee",
+                "Add role",
+                "Add department",
+                "Exit",
+            ]
+        }
+        //use case switch? make it easier with and cleaner code. 
+    ]).then(function (response) {
+        switch (response.todo) {
+            case "Add an employee?":
+                newEmployee();
+                break;
+
+            case "Add a role?":
+                newRole();
+                break;
+
+            case "Add a department?":
+                newDepartment();
+                break;
+
+            case "View all Employees?":
                 viewEmployees();
-            }
-            if (response.startToDo === "View all employees by department") {
+                break;
+
+            case "View all employees by department?":
                 viewDepartment();
-            }
-            if (response.startToDo === "View all employees by role") {
+                break;
+
+            case "View all employees by role?":
                 viewRole();
-            }
-            if (response.startToDo === "Add employee") {
-                newEmploy();
-            }
-            else if (response.ToDo === "Finished") {
-            console.log("You Have Finished!") 
-            }
-            else {
+                break;
+
+            case "Exit":
                 connection.end();
-            }
-        }) 
+                break;
+
+        }
+    })
 }
+
+async function newEmployee() {
+    const employeeResponses = await inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is your employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is your employee's last name?"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "What is the employee's role id?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the managers id?"
+        }
+
+
+    ]);
+    
+    var employee = { 
+        firstName: employeeResponses[0].name, 
+        lastName: employeeResponses[1].name, 
+        roleId: employeeResponses[2].name, 
+        managerId: employeeResponses[3].name}
+         connection.query(
+                "INSERT INTO employee SET ?",
+               {
+                    first_name = employee.firstName,
+                    last_name = employee.lastName,
+                    role_id = employee.roleId,
+                    manager_id = employee.managerId 
+              },
+                     function (error) {
+                    if (error) throw error;
+                    startToDo();
+                    connection.end();
+                });
+    
+    }
+    
+    
+
+
+
+                // use async return promises to recieve responses? and put into database?
 
